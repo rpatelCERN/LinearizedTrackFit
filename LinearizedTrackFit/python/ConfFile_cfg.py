5c04b96aa69ec481157ef7d44d6a90a911a9a5e1
Marco De Mattia <marco.de.mattia@cern.ch>
2014-12-04 00:37:18 +0100

    First commit

---
+++ LinearizedTrackFit/python/ConfFile_cfg.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.source = cms.Source("EmptySource")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+
+# process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+# process.source = cms.Source("PoolSource",
+#     # replace 'myfile.root' with the source file you want to use
+#     fileNames = cms.untracked.vstring(
+#         'file:myfile.root'
+#     )
+# )
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string("matrixHists.root") )
+
+process.demo = cms.EDAnalyzer('LinearizedTrackFit'
+)
+
+
+process.p = cms.Path(process.demo)
