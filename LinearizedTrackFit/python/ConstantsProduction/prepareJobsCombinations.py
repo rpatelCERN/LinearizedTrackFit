c850639ce968162cccbc4a075cd5cd4e5de66807
demattia <marco.de.mattia@cern.ch>
2015-06-19 16:17:20 -0500

    Scripts for generating all constants.

--- LinearizedTrackFit/python/ConstantsProduction/prepareJobsCombinations.py
+++ LinearizedTrackFit/python/ConstantsProduction/prepareJobsCombinations.py
@@ -11,31 +11,6 @@ class Combination:
         self.radius_cut_min = radius_cut_min
         self.radius_cut_max = radius_cut_max
         self.input_file_name = input_file_name
-        # self.set_radius_cuts()
-
-    # def set_radius_cuts(self):
-    #     first = -1
-    #     # Count the number of barrel layers
-    #     barrel_layers = 0
-    #     for l in self.layers:
-    #         if l < 11:
-    #             barrel_layers += 1
-    #     if barrel_layers >= 4:
-    #         return
-    #     # Add radius cuts based on the configuration
-    #     for i in range(len(self.layers)):
-    #         if 10 < self.layers[i] < 16:
-    #             if first == -1:
-    #                 self.radius_cut_min[self.layers[i]-5] = 61.
-    #                 # If we have 3 barrel layers we do not need to add extra cuts
-    #                 if barrel_layers >= 3:
-    #                     break
-    #                 first = i
-    #             else:
-    #                 self.radius_cut_min[self.layers[first]-5] = 0.
-    #                 self.radius_cut_max[self.layers[first]-5] = 61.
-    #                 self.radius_cut_min[self.layers[i]-5] = 61.
-    #                 break
 
 
 # Function to generate all combinations from a given set of 6 layers or disks
@@ -50,27 +25,38 @@ def generate_layers_combinations(base_name, layers_list, radius_cut_min, radius_
     return combinations
 
 
+def array_to_string(a):
+    s_a = ""
+    for x in a:
+        s_a += str(x)+","
+    return s_a.rstrip(",")
+
+
+
 # Function to generate the configuration file for a given combination
-def prepare_job(c, pt_min, pt_max, stub_coordinates, track_parameters, pre_estimate):
+def prepare_job(c, fit_type, oneOverPt_min, oneOverPt_max, stub_coordinates, track_parameters, pre_estimate):
     print "preparing files for", c.name
-    job_dir = "Combinations/"+c.name
+    job_dir = "Combinations/"+c.name+"/"
     pre_estimate_file_transverse = ""
     pre_estimate_file_longitudinal = ""
     if pre_estimate:
-        job_dir = "PreEstimate/"+c.name
+        job_dir = "PreEstimate_"+fit_type+"/"+c.name+"/"
     else:
-        pre_estimate_file_transverse = "PreEstimate/"+c.name+"/file.txt"
-        pre_estimate_file_longitudinal = "PreEstimate/"+c.name+"/file.txt"
+        pre_estimate_file_transverse = "PreEstimate_Transverse/"+c.name+"/file.txt"
+        pre_estimate_file_longitudinal = "PreEstimate_Longitudinal/"+c.name+"/file.txt"
     os.system("mkdir -p "+job_dir)
-    configuration_file = open("BuildLinearizedTrackFitMatrix_cfg.py", "w")
+
+    configuration_file = open(job_dir+"BuildLinearizedTrackFitMatrix_cfg.py", "w")
     for line in open("BuildLinearizedTrackFitMatrix_template_cfg.py"):
         line = line.replace("INPUT_FILE_NAME", c.input_file_name)
-        line = line.replace("ONE_OVER_PT_MIN", 1./pt_max)
-        line = line.replace("ONE_OVER_PT_MAX", 1./pt_min)
+        line = line.replace("ONE_OVER_PT_MIN", str(oneOverPt_min))
+        line = line.replace("ONE_OVER_PT_MAX", str(oneOverPt_max))
         line = line.replace("STUB_COORDINATES", stub_coordinates)
         line = line.replace("TRACK_PARAMETERS", track_parameters)
-        line = line.replace("CHARGE_REGIONS", 1)
-        line = line.replace("LAYERS_LIST", c.layers)
+        line = line.replace("CHARGE_REGIONS", "1")
+        line = line.replace("LAYERS_LIST", array_to_string(c.layers))
+        line = line.replace("RADIUS_CUTS_MIN", array_to_string(c.radius_cut_min))
+        line = line.replace("RADIUS_CUTS_MAX", array_to_string(c.radius_cut_max))
         line = line.replace("FIRST_ORDER_CHARGEOVERPT_COEFFICIENTS_FILE_NAME", pre_estimate_file_transverse)
         line = line.replace("FIRST_ORDER_COTTHETA_COEFFICIENTS_FILE_NAME", pre_estimate_file_longitudinal)
         configuration_file.write(line)
@@ -94,9 +80,9 @@ def prepare_job(c, pt_min, pt_max, stub_coordinates, track_parameters, pre_estim
 
 
 files_dir = "/home/demattia/fdata_demattia/Upgrade/LinearizedTrackFit/Stubs/Official/"
-file_barrel = files_dir+"Muons_Pt_2_more_Eta_m01p09_Phi_m05p1_3_z0_m15p15"
-file_hybrid = files_dir+"Muons_Pt_2_more_Eta_0p7_1p7_Phi_m05p1_3_z0_m15p15_HYBRID"
-file_endcaps = files_dir+"Muons_Pt_2_more_Eta_1p5_2p5_Phi_m05p1_3_z0_m15p15_ENDCAPS"
+file_barrel = files_dir+"Muons_Pt_2_more_Eta_m01p09_Phi_m05p1_3_z0_m15p15/extracted.root"
+file_hybrid = files_dir+"Muons_Pt_2_more_Eta_0p7_1p7_Phi_m05p1_3_z0_m15p15_HYBRID/extracted.root"
+file_endcaps = files_dir+"Muons_Pt_2_more_Eta_1p5_2p5_Phi_m05p1_3_z0_m15p15_ENDCAPS/extracted.root"
 
 
 combinations = []
@@ -160,20 +146,39 @@ radius_cut_max[15-5] = 61.
 combinations.extend(generate_layers_combinations(name, layers_list, radius_cut_min, radius_cut_max, file_endcaps))
 
 
-# cleaning
-os.system("rm -rf BackupPre_Transverse_Old; mv BackupPre_Transverse BackupPre_Transverse_Old")
-os.system("mkdir BackupPre_Transverse; mv PreEstimate_Transverse Backup")
-# Use the combinations to modify the cfg, generate directories with the given name, and submit the jobs.
-for c in combinations:
-    prepare_job(c, '"phi"', '"charge/pt"')
+class JobType:
+    def __init__(self, fit_type, stub_coordinates, track_parameters):
+        self.fit_type = fit_type
+        self.stub_coordinates = stub_coordinates
+        self.track_parameters = track_parameters
 
-os.system("rm -rf BackupPre_Longitudinal_Old; mv BackupPre_Longitudinal BackupPre_Longitudinal_Old")
-os.system("mkdir BackupPre_Longitudinal; mv PreEstimate_Longitudinal Backup")
-# Use the combinations to modify the cfg, generate directories with the given name, and submit the jobs.
-for c in combinations:
-    print c.name.split("_")[1]
-    prepare_job(c, '"phi"', '"charge/pt"')
+
+def prepare_pre_estimate_jobs(job_types, combinations):
+    for j in job_types:
+        os.system("rm -rf BackupPre_"+j.fit_type+"_Old; mv BackupPre_"+j.fit_type+" BackupPre_"+j.fit_type+"_Old")
+        os.system("mkdir BackupPre_"+j.fit_type+"; mv PreEstimate_"+j.fit_type+" BackupPre")
+        # Use the combinations to modify the cfg, generate directories with the given name, and submit the jobs.
+        for c in combinations:
+            # pT > 2 GeV/c
+            prepare_job(c, j.fit_type, 0., 1./2., j.stub_coordinates, j.track_parameters, True)
+
+
+
+job_types = [JobType("Transverse", '"phi"', '"charge/pt"')]
+job_types.append(JobType("Longitudinal", '"z"', '"cotTheta"'))
+job_types.append(JobType("Longitudinal", '"R", "z"', '"cotTheta"'))
+
+prepare_pre_estimate_jobs(job_types, combinations)
+
 
 
 
 os.system("rm -rf Backup_Old; mv Backup Backup_Old; mkdir Backup; mv Combinations Backup")
+
+
+
+
+# prepare_job(c, "Transverse_lowPt", 1./10., 1./2., '"phi"', '"charge/pt"', True)
+# prepare_job(c, "Transverse_highPt", 0., 1./10., '"phi"', '"charge/pt"', True)
+# prepare_job(c, "Transverse_lowPt_15", 1./15., 1./2., '"phi"', '"charge/pt"', True)
+# prepare_job(c, "Transverse_highPt_15", 0., 1./15., '"phi"', '"charge/pt"', True)
