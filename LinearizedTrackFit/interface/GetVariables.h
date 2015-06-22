a4f90620b144e9a00224454a2fe2e9edb4387627
demattia <marco.de.mattia@cern.ch>
2015-06-22 17:16:48 -0500

    Corrected 2D plots.

--- LinearizedTrackFit/interface/GetVariables.h
+++ LinearizedTrackFit/interface/GetVariables.h
@@ -1755,7 +1755,7 @@ public:
   virtual double at(const int k, const std::map<int, unsigned int> & layersFound) {
     double R = std::sqrt(std::pow(var_x->at(k), 2) + std::pow(var_y->at(k), 2));
     double DeltaR = R - meanRadius(var_layer->at(k));
-    double cotTheta = cotThetaEstimator_.estimate(var_z, layersFound);
+    double cotTheta = cotThetaEstimator_.estimate(var_x, var_y, var_z, layersFound);
     double oneOverRho = (3.8114*0.003)*chargeOverPtEstimator_.estimate(var_x, var_y, layersFound);
     return (var_z->at(k) - (DeltaR + 1/24.*std::pow(R, 3)*(oneOverRho*oneOverRho))*cotTheta);
     // double DeltaRCube = std::pow(R, 3) - std::pow(meanRadius(var_layer->at(k)), 3);
